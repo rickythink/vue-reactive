7512ea1e31b19afe39edcfea82df6df5141ae9a8
ricky <wqscut@gmail.com>
2019-12-12 17:36:22 +0800

    test: 增加重复对reactive变量调用的测试

--- src/__tests__/reactive.ts
+++ src/__tests__/reactive.ts
@@ -1,13 +1,23 @@
 import { reactive } from '../index'
 
 describe('reactive', () => {
-  test('should cache observe', () => {
+  test('should return same with repeat target reactive', () => {
     const data = {
       msg: 'Hello World',
       number: 1
     }
     const observe1 = reactive(data)
     const observe2 = reactive(data)
+    expect(observe1 === observe2).toBeTruthy()
+  })
+
+  test('should return same with repeat reactive', () => {
+    const data = {
+      msg: 'Hello World',
+      number: 1
+    }
+    const observe1 = reactive(data)
+    const observe2 = reactive(observe1)
     expect(observe1 === observe2).toBeTruthy()
   })
 })
--- src/reactive.ts
+++ src/reactive.ts
@@ -1,15 +1,25 @@
 import { handlers } from './handler'
 
+/**
+ * [observed, target]
+ */
 const reactiveToRaw = new WeakMap<any, any>()
+/**
+ * [target, observed]
+ */
 const reactiveToProxy = new WeakMap<any, any>()
 
+/**
+ * reactive
+ * @param target 要响应式监听的对象
+ */
 export function reactive<T extends object>(target: T) {
   let observed = reactiveToProxy.get(target)
   // 如果是缓存代理过的
   if (observed) {
     return observed
   }
-  // 重复调用reactive
+  // 重复调用reactive(target与之前的observed相同)
   if (reactiveToRaw.has(target)) {
     return target
   }
