ecb644f000860dc1eb20689e8955164051afe519
ricky <wqscut@gmail.com>
2019-12-12 18:02:20 +0800

    refactor: 为重要的函数和变量添加注释

--- src/effect.ts
+++ src/effect.ts
@@ -1,21 +1,52 @@
 interface ReactiveEffect<T = any> {
+  /**
+   * effect 函数
+   */
   (): T
-  deps: Array<Dep>
+  /**
+   * 选项
+   */
   options: ReactiveEffectOptions
 }
 
 interface ReactiveEffectOptions {
+  /**
+   * 是否立即执行 effect 包裹的函数 fn
+   */
   lazy?: boolean
   computed?: boolean
-  onStop?: () => void
 }
 
 type Dep = Set<ReactiveEffect>
 type KeyToDepMap = Map<any, Dep>
-// 存储effect
+/**
+ * 所有响应式监听的变量Map
+ *
+ * reactive 变量只要有 get 就会被 track() 依赖收集加入 targetMap
+ * @example
+ * ```
+ * targetMap = [
+ *  <target>: {
+ *    <key>: [<effect1>,<effect2>,...]
+ *  }
+ * ]
+ * ```
+ */
 const targetMap = new WeakMap<any, KeyToDepMap>()
+
+/**
+ * effect 栈
+ *
+ * - effect 没有`lazy` 选项时，effect 因为执行而入栈->执行->出栈
+ * - 有`lazy`选项，effect 会存在 effectStack 中
+ */
 const effectStack: ReactiveEffect[] = []
 
+/**
+ * effect 顶层函数
+ * @param fn 包裹响应式变量的函数
+ * @param options 响应式监听的参数
+ */
 export function effect(fn: () => any, options: ReactiveEffectOptions = {}) {
   const effect = createReactiveEffect(fn, options)
   if (!options.lazy) {
@@ -24,15 +55,27 @@ export function effect(fn: () => any, options: ReactiveEffectOptions = {}) {
   return effect
 }
 
+/**
+ * 实际创建 effect 的函数
+ * @param fn 包裹响应式变量的函数
+ * @param options 响应式监听的参数
+ */
 function createReactiveEffect(fn: () => any, options: ReactiveEffectOptions) {
   const effect = function reactiveEffect(...args: unknown[]): unknown {
     return run(effect, fn, args)
   } as ReactiveEffect
   effect.options = options
-  effect.deps = []
   return effect
 }
 
+/**
+ * effect 执行栈管理相关
+ * @param effect effect 函数本身
+ * @param fn effect 包裹的函数
+ * @param args effect 额外的参数
+ *
+ * @description finally 语句块一定会执行
+ */
 function run(effect: ReactiveEffect, fn: Function, args: unknown[]): unknown {
   if (effectStack.indexOf(effect) === -1) {
     try {
@@ -44,8 +87,13 @@ function run(effect: ReactiveEffect, fn: Function, args: unknown[]): unknown {
   }
 }
 
-// 跟踪订阅effect
-export function track(target: object, key: unknown) {
+/**
+ * 依赖收集
+ * @param target 被依赖收集的对象
+ * @param key 被依赖收集的对象key
+ */
+export function track(target: object, key: string | symbol) {
+  // 获取最新入栈的 effect
   const effect = effectStack[effectStack.length - 1]
   if (effect) {
     let depsMap = targetMap.get(target)
@@ -61,13 +109,16 @@ export function track(target: object, key: unknown) {
     if (!dep.has(effect)) {
       // 收集当前的effect
       dep.add(effect)
-      // effect 收集当前的dep
-      effect.deps.push(dep)
     }
   }
 }
 
-export function trigger(target: object, key?: unknown) {
+/**
+ * 触发更新
+ * @param target 触发更新的对象
+ * @param key 触发更新的对象的key
+ */
+export function trigger(target: object, key?: string | symbol) {
   const depsMap = targetMap.get(target)
   // 没有被订阅到
   if (depsMap === undefined) {
--- src/handler.ts
+++ src/handler.ts
@@ -5,12 +5,13 @@ import { track, trigger } from './effect'
 export const handlers = {
   get(target: object, key: string | symbol, receiver: object) {
     const res = Reflect.get(target, key, receiver)
-    // effect 收集
+    // 依赖收集
     track(target, key)
     return isObject(res) ? reactive(res) : res
   },
   set(target: object, key: string | symbol, value: unknown, receiver: object) {
     const result = Reflect.set(target, key, value, receiver)
+    // 触发更新
     trigger(target, key)
     return result
   }
